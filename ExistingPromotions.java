c36a8fcba49f32b74962519168a766e6e4d4f6aa
vaibhavmanit <vaibhavmanit@users.noreply.github.com>
2020-09-27 13:20:27 +0530

    Update ExistingPromotions.java

--- ExistingPromotions.java
+++ ExistingPromotions.java
@@ -1,5 +1,6 @@
 package com.java.promotionengine.promotions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.java.promotionengine.sku.SKUId;
@@ -8,6 +9,15 @@ public class ExistingPromotions {
 	List<Promotions> existingPromotions;
 
 	public ExistingPromotions() {
+		existingPromotions = new ArrayList<Promotions>();
+		Promotions promotionA = new Promotions(new SKUId[] {SKUId.A}, new int [] {3});
+		existingPromotions.add(promotionA);
+		
+		Promotions promotionB = new Promotions(new SKUId[] {SKUId.B}, new int [] {2});
+		existingPromotions.add(promotionB);
+		
+		Promotions promotionCD = new Promotions(new SKUId[] {SKUId.C,SKUId.D}, new int [] {1,1});
+		existingPromotions.add(promotionCD);
 		
 	}
 	
@@ -19,7 +29,7 @@ public class ExistingPromotions {
 		this.existingPromotions = existingPromotions;
 	}
 	
-	public void addPromotion(SKUId sKUId, int number){
+	public void addPromotion(SKUId []sKUId, int []number){
 		existingPromotions.add(new Promotions(sKUId, number));
 		
 	}
