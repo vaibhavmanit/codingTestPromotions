6364477dfeb3c865a450d9b1eff7196ff0baf0e8
vaibhavmanit <vaibhavmanit@users.noreply.github.com>
2020-09-27 23:16:03 +0530

    Update ExistingPromotions.java

--- ExistingPromotions.java
+++ ExistingPromotions.java
@@ -6,17 +6,18 @@ import java.util.List;
 import com.java.promotionengine.sku.SKUId;
 
 public class ExistingPromotions {
-	List<Promotions> existingPromotions;
+	private List<Promotions> existingPromotions;
+	
 
 	public ExistingPromotions() {
 		existingPromotions = new ArrayList<Promotions>();
-		Promotions promotionA = new Promotions(new SKUId[] {SKUId.A}, new int [] {3});
+		Promotions promotionA = new Promotions(new SKUId[] {SKUId.A}, new int [] {3}, 130);
 		existingPromotions.add(promotionA);
 		
-		Promotions promotionB = new Promotions(new SKUId[] {SKUId.B}, new int [] {2});
+		Promotions promotionB = new Promotions(new SKUId[] {SKUId.B}, new int [] {2}, 45);
 		existingPromotions.add(promotionB);
 		
-		Promotions promotionCD = new Promotions(new SKUId[] {SKUId.C,SKUId.D}, new int [] {1,1});
+		Promotions promotionCD = new Promotions(new SKUId[] {SKUId.C,SKUId.D}, new int [] {1,1}, 20);
 		existingPromotions.add(promotionCD);
 		
 	}
@@ -29,9 +30,25 @@ public class ExistingPromotions {
 		this.existingPromotions = existingPromotions;
 	}
 	
-	public void addPromotion(SKUId []sKUId, int []number){
-		existingPromotions.add(new Promotions(sKUId, number));
+	public void addPromotion(SKUId []sKUId, int []number, int costForPromotion){
+		existingPromotions.add(new Promotions(sKUId, number,costForPromotion));
 		
+	}
+	
+	public void activePromotions(ExistingPromotions existPromo) {
+		
+		for(Promotions promotion: existPromo.getExistingPromotions()) {
+			for(int count = 0; count< promotion.getsKUId().length ; count++) {
+				if(count+1==promotion.getsKUId().length) {
+					System.out.println(promotion.getNumberOfSKUid()[count] + " of " + promotion.getsKUId()[count]
+						+ "'s for " + promotion.getCostOfPromotion());
+					System.out.println();
+				}else {
+					System.out.print(promotion.getNumberOfSKUid()[count] + " of " + promotion.getsKUId()[count]
+							+ "'s and ");
+				}
+			}
+		}
 	}
 	
 	public void deletePromotion(int promotionToBeRemoved){
@@ -39,4 +56,5 @@ public class ExistingPromotions {
 		existingPromotions.remove(promotionToBeRemoved);
 		
 	}
+	
 }
